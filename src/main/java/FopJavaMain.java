2d8c0013fe474945f93efd7709fa378a93eb501e
latinovits_xund <latinovits@xund.ai>
2025-08-22 13:30:43 +0200

    add examples

--- src/main/java/FopJavaMain.java
+++ src/main/java/FopJavaMain.java
@@ -1,22 +1,38 @@
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class FopJavaMain {
   public static void main(String[] args) {
     try {
       var impl = new FopImpl();
-      var inputBytes = Files.readAllBytes(Path.of("input.xml"));
-      var configBytes = Files.readAllBytes(Path.of("config.xml"));
-      var fontBytes = Files.readAllBytes(Path.of("Roboto.ttf"));
-      impl.setFile("file:///input.xml", new String(inputBytes, StandardCharsets.ISO_8859_1));
-      impl.setFile("file:///config.xml", new String(configBytes, StandardCharsets.ISO_8859_1));
-      impl.setFile("file:///Roboto.ttf", new String(fontBytes, StandardCharsets.ISO_8859_1));
-      var output = impl.transform("file:///input.xml", "file:///config.xml");
-      Files.write(Paths.get("output-java.pdf"), output);
+
+      var files = Files.walk(Path.of("examples")).toList();
+
+      for (Path p : files) {
+        var file = new File(p.toUri());
+        if (!file.isDirectory()) {
+          System.out.println("Adding file " + p);
+          var bytes = Files.readAllBytes(p);
+          impl.setFile("file:///" + p, new String(bytes, StandardCharsets.ISO_8859_1));
+        }
+      }
+
+      for (Path p : files) {
+        if (p.toString().endsWith(".fo")) {
+          var outputPath = Path.of(p.toString().replace(".fo", "-java.pdf"));
+          if (outputPath.equals(p)) {
+            throw new RuntimeException("output path equals input path");
+          }
+          System.out.println("Converting " + p+ " to " + outputPath);
+          var output = impl.transform("file:///" + p, "file:///examples/config.xconf");
+          Files.write(outputPath, output);
+        }
+      }
     } catch (Throwable e) {
       throw new RuntimeException(e);
     }
   }
+
 }
